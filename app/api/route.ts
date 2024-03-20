f4ad58b7e18f7354c3afcb0f5359a300f04a3abc
Sepehr Safari <safari.sepehr@gmail.com>
2024-03-20 23:27:00 +0330

    add payment

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,12 +1,21 @@
+import { webln } from '@getalby/sdk';
+import NDK, { NDKPrivateKeySigner } from '@nostr-dev-kit/ndk';
 import { load } from 'cheerio';
+import 'websocket-polyfill';
 
 export const dynamic = 'force-dynamic';
 
 export async function GET(request: Request) {
   let isFollowing = false;
+  let isPaid = false;
 
   const { searchParams } = new URL(request.url);
   const un = searchParams.get('un')?.toLocaleLowerCase() || '';
+  const pk = searchParams.get('pk') || '';
+
+  if (!un || !pk) {
+    return new Response('Missing parameters', { status: 400 });
+  }
 
   const res = await fetch(`https://github.com/sepehr-safari?tab=followers`);
   const html = await res.text();
@@ -20,7 +29,34 @@ export async function GET(request: Request) {
     }
   });
 
-  return new Response(JSON.stringify({ isFollowing }), {
+  if (!isFollowing) {
+    return new Response(JSON.stringify({ isFollowing, isPaid }), {
+      headers: { 'content-type': 'application/json' },
+    });
+  }
+
+  const nwc = new webln.NWC({ nostrWalletConnectUrl: process.env.NWC_URL });
+  await nwc.enable();
+
+  const ndk = new NDK({
+    signer: new NDKPrivateKeySigner(process.env.NDK_PRIVATE_KEY),
+    autoConnectUserRelays: false,
+    autoFetchUserMutelist: false,
+    explicitRelayUrls: ['wss://relay.getalby.com', 'wss://nos.lol', 'wss://relay.nostr.band'],
+  });
+  await ndk.connect();
+  const user = ndk.getUser({ pubkey: pk });
+  const invoice = await user.zap(1000);
+  if (!invoice) {
+    return new Response('Error creating invoice', { status: 500 });
+  }
+
+  const payRes = await nwc.sendPayment(invoice);
+  if (payRes) {
+    isPaid = true;
+  }
+
+  return new Response(JSON.stringify({ isFollowing, isPaid }), {
     headers: { 'content-type': 'application/json' },
   });
 }
